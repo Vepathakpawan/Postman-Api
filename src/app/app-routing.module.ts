c26583961714fbbaaf5572c76b6cd4eb3971e66a
Vepathakpawan <pawanved007@gmail.com>
2021-06-05 09:09:36 -0700

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import{ AddComponent} from './add/add.component';
+import{ ListComponent} from './list/list.component';
+import{ UpdateComponent}from './update/update.component';
+const routes: Routes = [
+
+  {
+    path:'add',
+    component: AddComponent
+  },
+  {
+    path:'list',
+    component: ListComponent
+  },
+  {
+    path:'update',
+    component:UpdateComponent
+  }
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AddComponent } from './add/add.component';
+import { UpdateComponent } from './update/update.component';
+import { ListComponent } from './list/list.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddComponent,
+    UpdateComponent,
+    ListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
